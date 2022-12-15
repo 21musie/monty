04eb80be78eaec28fd230fd11482158ac6b32530
21musie <104737346+21musie@users.noreply.github.com>
2022-12-16 02:21:37 +0300

    uplode on monty

--- main.c
+++ main.c
@@ -1,103 +1,126 @@
 #include "monty.h"
 
-global_t vglo;
+/* Initialize the global variable */
+int token = 1;
 
 /**
- * free_vglo - frees the global variables
- *
- * Return: no return
- */
-void free_vglo(void)
-{
-	free_dlistint(vglo.head);
-	free(vglo.buffer);
-	fclose(vglo.fd);
-}
-
-/**
- * start_vglo - initializes the global variables
- *
- * @fd: file descriptor
- * Return: no return
- */
-void start_vglo(FILE *fd)
-{
-	vglo.lifo = 1;
-	vglo.cont = 1;
-	vglo.arg = NULL;
-	vglo.head = NULL;
-	vglo.fd = fd;
-	vglo.buffer = NULL;
-}
-
-/**
- * check_input - checks if the file exists and if the file can
- * be opened
- *
- * @argc: argument count
- * @argv: argument vector
- * Return: file struct
- */
-FILE *check_input(int argc, char *argv[])
-{
-	FILE *fd;
-
-	if (argc == 1 || argc > 2)
-	{
-		dprintf(2, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
-
-	fd = fopen(argv[1], "r");
-
-	if (fd == NULL)
-	{
-		dprintf(2, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-
-	return (fd);
-}
-
-/**
- * main - Entry point
- *
- * @argc: argument count
- * @argv: argument vector
+ * main - Interprets bytecode
+ * @argc: number of arguments
+ * @argv: array of arguments
  * Return: 0 on success
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	void (*f)(stack_t **stack, unsigned int line_number);
-	FILE *fd;
-	size_t size = 256;
-	ssize_t nlines = 0;
-	char *lines[2] = {NULL, NULL};
+	const char *filename;
+	char *string = NULL, *opcode, *num_str;
+	size_t nbytes = 1;
+	FILE *file;
+	unsigned int line_num = 0, i = 0;
+	ssize_t read_c = 0;
+	stack_t *stack;
 
-	fd = check_input(argc, argv);
-	start_vglo(fd);
-	nlines = getline(&vglo.buffer, &size, fd);
-	while (nlines != -1)
+	stack = NULL;
+	if (argc != 2)
 	{
-		lines[0] = _strtoky(vglo.buffer, " \t\n");
-		if (lines[0] && lines[0][0] != '#')
-		{
-			f = get_opcodes(lines[0]);
-			if (!f)
-			{
-				dprintf(2, "L%u: ", vglo.cont);
-				dprintf(2, "unknown instruction %s\n", lines[0]);
-				free_vglo();
-				exit(EXIT_FAILURE);
-			}
-			vglo.arg = _strtoky(NULL, " \t\n");
-			f(&vglo.head, vglo.cont);
-		}
-		nlines = getline(&vglo.buffer, &size, fd);
-		vglo.cont++;
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
 	}
 
-	free_vglo();
+	filename = argv[1];
+
+	/* Open File with the bytecodes */
+	file = fopen(filename, "r");
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", filename);
+		exit(EXIT_FAILURE);
+	}
+
+	/* Read file line by line */
+	while (read_c != EOF)
+	{
+		token = 1;
+		i = 0;
+		if (string != NULL)
+			free(string);
+		string = NULL;
+
+		read_c = getline(&string, &nbytes, file);
+
+		if (read_c == -1)
+		{
+			free(string);
+			if (stack != NULL)
+				free_stack(&stack);
+			fclose(file);
+			return (0);
+		}
+
+		/* Keep count of the number of lines */
+		line_num++;
+
+		/* Continue if line or string is NULL */
+		if (read_c == 0)
+			continue;
+
+		/* Continue if line had only the new line character */
+		if (read_c == 1)
+			continue;
+
+		/* Parse the first elements of the line */
+		opcode = strtok(string, " \n");
+
+		/* If string is empty, let's continue */
+		if (opcode == NULL)
+			continue;
+
+		if (opcode[0] == '#')
+		{
+			nop(&stack, line_num);
+			continue;
+		}
+
+		/* Check whether the first token is the opcode 'push' */
+		if (strcmp(opcode, "push") == 0)
+		{
+			num_str = strtok(NULL, " \n");
+
+			/* Check if token is a digit or NULL */
+			if (num_str == NULL)
+			{
+				fprintf(stderr, "L%d: usage: push integer\n",
+					line_num);
+				free(string);
+				free_stack(&stack);
+				fclose(file);
+				exit(EXIT_FAILURE);
+			}
+
+			if (num_str[0] == '-' && num_str[1] != '\0')
+				i = 1;
+			/* Make sure string isn't garbage */
+			for (; num_str[i] != '\0'; i++)
+			{
+				if (isdigit(num_str[i]) == 0)
+				{
+					fprintf(stderr,
+						"L%d: usage: push integer\n",
+						line_num);
+					free(string);
+					free_stack(&stack);
+					fclose(file);
+					exit(EXIT_FAILURE);
+				}
+			}
+
+			token = atoi(num_str);
+		}
+		op_func(opcode, &stack, line_num)(&stack, line_num);
+	}
+	/* Free memory and close the file */
+	free(string);
+	free_stack(&stack);
+	fclose(file);
 
 	return (0);
 }
